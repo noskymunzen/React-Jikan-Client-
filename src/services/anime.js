4571aaedc98768d3a17241616c6cbd5e899be34a
noskymunzen <munzenmayer@outlook.com>
2022-03-31 17:12:23 -0300

    adds service anime

---
+++ src/services/anime.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.jikan.moe/v4",
+});
+
+const getAnime = async (search = "") => {
+  const endpoint = `/anime?q=${search}`;
+  const { data } = await instance.get(endpoint);
+  return data;
+};
+
+const getAnimeById = async (id) => {
+  const endpoint = `/anime/${id}`;
+  const { data } = await instance.get(endpoint);
+  return data;
+};
+
+export default {
+  getAnime,
+  getAnimeById,
+};
