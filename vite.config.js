6ac67d9bdd8aedc3eedaf2df0e280f95de52f34e
noskymunzen <munzenmayer@outlook.com>
2022-04-08 22:17:07 -0400

    feat: config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "",
   build: {
     outDir: path.join(__dirname, "build"),
   },
